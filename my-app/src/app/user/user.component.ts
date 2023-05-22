88c1e2ecdd899822fc6f76abcb4f51846b43611e
kkprajapati <kuldip.code@gmail.com>
2023-05-22 15:28:29 +0530

    login updates

--- my-app/src/app/user/user.component.ts
+++ my-app/src/app/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
