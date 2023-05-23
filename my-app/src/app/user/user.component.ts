1a52f4d0186b0acd4397240ceba074ab9fdb1bd4
kkprajapati <kuldip.code@gmail.com>
2023-05-23 16:12:12 +0530

    signup

--- my-app/src/app/user/user.component.ts
+++ my-app/src/app/user/user.component.ts
@@ -1,18 +1,52 @@
-import { Component,OnInit } from '@angular/core';
-import { FormGroup, FormControl,Validators, FormBuilder } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit{
-  user:any; 
-  formdata:any;
+export class UserComponent implements OnInit {
+  firstname: string;
+  lastname: string;
+  email: string;
+  profileimage: string;
+  password:string;
+  private user = {};
+
+  constructor(private httpClient: HttpClient) { }
+
+  formdata: any;
+  private Url = 'http://localhost:3000/api/user/signup';
+  private httpOptions = {
+     headers: new HttpHeaders( { 'Content-Type': 'application/json' })
+  };
+  sendPostRequest(data: any): Observable<any> {
+    return this.httpClient.post<any>(this.Url, data);
+}
   ngOnInit() {
-    this.formdata = new FormGroup({ 
-      user: new FormControl("user")
-   }); 
+    this.formdata = new FormGroup({
+      firstname: new FormControl("user"),
+      lastname: new FormControl("user"),
+      email: new FormControl("user"),
+      profileimage: new FormControl("user"),
+      password: new FormControl("user"),
+    });
   }
-  onClickSubmit(data:any) {this.user = data.user; console.log(data)}
+  onClickSubmit(data: any) {
+    this.firstname = data.firstname;
+    this.lastname = data.lastname;
+    this.email = data.email;
+    this.profileimage = data.profileimage;
+    this.password = data.password;
+
+    console.log(data);
+    this.sendPostRequest(data).subscribe(
+      res => {
+        console.log(res);
+      }
+);
+}
 }
