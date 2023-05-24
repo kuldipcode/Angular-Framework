11076ae432dca68db9be6e2ed0ec6d6bdbe9c339
kkprajapati <kuldip.code@gmail.com>
2023-05-24 18:53:55 +0530

    login

--- my-app/src/app/login/login.component.ts
+++ my-app/src/app/login/login.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  email: string;
+  password:string;
+  private user = {};
 
+  constructor(private httpClient: HttpClient) { }
+
+  formdata: any;
+  private Url = 'http://localhost:3000/api/user/login';
+  private httpOptions = {
+     headers: new HttpHeaders( { 'Content-Type': 'application/json' })
+  };
+  sendPostRequest(data: any): Observable<any> {
+    return this.httpClient.post<any>(this.Url, data);
 }
+  ngOnInit() {
+    this.formdata = new FormGroup({
+      email: new FormControl("kuldip.code@gmail.com"),
+      password: new FormControl("tempPass"),
+    });
+  }
+  onClickSubmit(data: any) {
+    this.email = data.email;
+    this.password = data.password;
+
+    console.log(data);
+    this.sendPostRequest(data).subscribe(
+      res => {
+        console.log(res);
+      }
+);
+}
+}
+
